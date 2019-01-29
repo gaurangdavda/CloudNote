56fe3ed7457ba2978e9b6cb061ecda9ba04c136e
Mansi <raghuwanshi.m@husky.neu.edu>
2019-01-29 10:30:27 -0500

    Annotated the User credential model

--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/model/UserCredentials.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/model/UserCredentials.java
@@ -1,7 +1,28 @@
 package edu.neu.csye6225.spring19.cloudninja.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="user")
 public class UserCredentials {
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Id
+	@GeneratedValue
+	@Column(name="user_id")
+	private int id;
+
+	@Column(name="user_emailId")
 	private String emailId;
+
+	@Column(name="user_password")
 	private String password;
 
 	public String getEmailId() {
