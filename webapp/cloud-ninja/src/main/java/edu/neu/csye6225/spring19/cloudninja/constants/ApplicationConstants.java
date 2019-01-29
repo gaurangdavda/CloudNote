3278d2597f09bce8b8e852a8413747895a8f0631
Gaurang Davda <davda.g@husky.neu.edu>
2019-01-28 21:13:45 -0500

    Email regex & exception handling

---
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/constants/ApplicationConstants.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package edu.neu.csye6225.spring19.cloudninja.constants;
+
+/**
+ * @author gaurang
+ *
+ */
+public class ApplicationConstants {
+
+	public static final String NULL_PASSWORD = "Kindly enter the password.";
+	public static final String WEAK_PASSWORD = "Password is weak. Kindly enter the password with the following specifications."
+			+ "\n 1) Length should be >= 8 and <= 50." + "\n 2) Should contain atleast 1 lower case character."
+			+ "\n 3) Should contain atleast 1 upper case character." + "\n 4) Should contain atleast 1 digit.";
+	public static final String NULL_EMAIL = "Kindly enter the emaild id.";
+	public static final String INVALID_EMAIL = "Kindly enter valid emaild id.";
+}
