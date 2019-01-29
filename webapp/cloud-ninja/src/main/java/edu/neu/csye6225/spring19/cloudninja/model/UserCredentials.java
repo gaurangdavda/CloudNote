157692e630ddcd712345273780c5d9365e558029
Mansi <raghuwanshi.m@husky.neu.edu>
2019-01-29 18:14:14 -0500

    Small code changes

--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/model/UserCredentials.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/model/UserCredentials.java
@@ -31,15 +31,7 @@ public class UserCredentials {
 	}
 
 
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
