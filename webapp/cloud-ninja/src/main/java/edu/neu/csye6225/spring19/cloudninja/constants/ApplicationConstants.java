f4e1db5b4dadb74ee76a78ce29a98fec39d95233
Gaurang Davda <davda.g@husky.neu.edu>
2019-01-28 22:58:49 -0500

    adding proper constants

--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/constants/ApplicationConstants.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/constants/ApplicationConstants.java
@@ -11,9 +11,16 @@ public class ApplicationConstants {
 
 	public static final String NULL_PASSWORD = "Kindly enter the password.";
 	public static final String WEAK_PASSWORD = "Password is weak. Kindly enter the password with the following specifications."
-			+ "\n 1) Length should be >= 8 and <= 50." + "\n 2) Should contain atleast 1 lower case character."
-			+ "\n 3) Should contain atleast 1 upper case character." + "\n 4) Should contain atleast 1 digit.";
+			+ "\n1) Length should be >= 8 and <= 50." + "\n2) Should contain atleast 1 lower case character."
+			+ "\n3) Should contain atleast 1 upper case character." + "\n4) Should contain atleast 1 digit.";
 	public static final String NULL_EMAIL = "Kindly enter the emaild id.";
 	public static final String INVALID_EMAIL = "Kindly enter valid emaild id.";
-	public static final String PASSWORD_INCORRECT = "PKindly enter valid credentials";
+	public static final String PASSWORD_INCORRECT = "Kindly enter valid password";
+	public static final String GET_ENDPOINT = "/";
+	public static final String POST_ENDPOINT = "/user/register";
+	public static final String HTTP_OK = "User Created Successfully";
+	public static final String AUTH = "Authorization";
+	public static final String NO_AUTH = "No Auth";
+	public static final String EMAILID_PASSWORD_MISSING = "EmailId/Username or Password not entered";
+	public static final String EMAILID_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
 }
--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/LoginService.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/LoginService.java
@@ -4,6 +4,7 @@ import edu.neu.csye6225.spring19.cloudninja.exception.ValidationException;
 import edu.neu.csye6225.spring19.cloudninja.model.UserCredentials;
 
 public interface LoginService {
+
 	public String getTimestamp(String authHeader) throws ValidationException;
 
 	public String registerUser(UserCredentials userCredentials) throws ValidationException;
