90a81fa7cd74c7d8ac261d0c958da3897c608d07
Mansi <raghuwanshi.m@husky.neu.edu>
2019-01-29 18:19:03 -0500

    Junit for register user

---
+++ webapp/cloud-ninja/src/test/java/edu/neu/csye6225/spring19/cloudninja/service/impl/LoginServiceImplTest.java
@@ -0,0 +1,12 @@
+package edu.neu.csye6225.spring19.cloudninja.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginServiceImplTest {
+
+    @Test
+    public void registerUser() {
+    }
+}
