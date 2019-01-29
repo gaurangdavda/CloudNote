6478ab5fb647451c2b67e5207c61295b90b851ee
vivdalal <dalal.vi@husky.neu.edu>
2019-01-28 16:28:41 -0800

    Added basic authentication

--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/LoginService.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/LoginService.java
@@ -1,7 +1,5 @@
 package edu.neu.csye6225.spring19.cloudninja.service;
 
-import edu.neu.csye6225.spring19.cloudninja.model.UserCredentials;
-
 public interface LoginService {
-	public boolean checkCredentials(UserCredentials userCredentials);
+	public boolean checkCredentials(String authHeader);
 }
--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/impl/LoginServiceImpl.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/impl/LoginServiceImpl.java
@@ -1,15 +1,21 @@
 package edu.neu.csye6225.spring19.cloudninja.service.impl;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Service;
 
-import edu.neu.csye6225.spring19.cloudninja.model.UserCredentials;
 import edu.neu.csye6225.spring19.cloudninja.service.LoginService;
 
 @Service
 public class LoginServiceImpl implements LoginService{
 
 	@Override
-	public boolean checkCredentials(UserCredentials userCredentials) {
+	public boolean checkCredentials(String authHeader) {
+		//HttpHeaders headers = new HttpHeaders();
+		byte[] bytes = Base64.decodeBase64(authHeader.split(" ")[1]);
+		String userPassArr[] = new String(bytes).split(":");
+		
+		System.out.println("Username:"  + userPassArr[0]);
+		System.out.println("Password:"  + userPassArr[1]);
 		
 		return true;
 	}
