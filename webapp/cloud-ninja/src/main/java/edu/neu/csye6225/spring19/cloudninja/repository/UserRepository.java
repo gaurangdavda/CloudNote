49ad31efc432eafb19a90933dd3f5cfeaa0768f8
vivdalal <dalal.vi@husky.neu.edu>
2019-01-29 18:51:50 -0800

    Updated repository

--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/repository/UserRepository.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package edu.neu.csye6225.spring19.cloudninja.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import edu.neu.csye6225.spring19.cloudninja.model.UserCredentials;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository  extends JpaRepository<UserCredentials,Integer> {
+@RepositoryRestResource
+public interface UserRepository  extends CrudRepository<UserCredentials,Integer> {
 
-
+	@Query("select u from USR_DTLS u where u.USR_EMAIL = ?")
+    public List<UserCredentials> findByEmailId (String emailId);
 
 }
