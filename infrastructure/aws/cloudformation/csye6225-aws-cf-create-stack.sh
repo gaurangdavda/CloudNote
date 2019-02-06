47500eb38739a8f8642bf7f2ea715dd824619b9d
vivdalal <dalal.vi@husky.neu.edu>
2019-02-05 21:51:53 -0500

    Paramertized Cloud Formation template and fixed bugs

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -7,10 +7,16 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
+#Replacing the STACK_NAME passed by the user in the csye6225-cf-networking-parameters.json
+sed -i "s/REPLACE_STACK_NAME/$1/g" csye6225-cf-networking-parameters.json
+
 ##Creating Stack
 echo "Creating Cloud Stack $1"
 response=$(aws cloudformation create-stack --stack-name "$1" --template-body file://csye6225-cf-networking.json --parameters file://csye6225-cf-networking-parameters.json)
+echo $?
 echo "Waiting for Stack $1 to be created"
 echo "$response"
+
 aws cloudformation wait stack-create-complete --stack-name $1
-echo "stack $1 is created"
+echo "stack $1 created successfully"
+sed -i "s/$1/REPLACE_STACK_NAME/g" csye6225-cf-networking-parameters.json
