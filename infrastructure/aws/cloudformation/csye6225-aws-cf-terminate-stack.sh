375a8257c0724c9dab34072c34b44c4702bdb60d
Mansi <raghuwanshi.m@husky.neu.edu>
2019-02-06 20:10:29 -0500

    Updated termination script

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 #script to delete AWS stack
+
+set -e
+if [ $# -lt 1 ]; then
+  echo "Kindly provide stack name! Script execution stopped."
+  exit 1
+fi
+
+#Replacing the STACK_NAME passed by the user in the csye6225-cf-networking-parameters.json
+sed -i "s/REPLACE_STACK_NAME/$1/g" csye6225-cf-networking-parameters.json
+
 echo " "
 echo "----Deleting Stack----"
 echo " "
 echo " "
-#Getting stack name from user 
-echo "Enter the name for the stack"
-read stack_name
-aws cloudformation delete-stack --stack-name $stack_name
+aws cloudformation delete-stack --stack-name $1
+echo "----Waiting for the stack $1 to be deleted----"
+aws cloudformation wait stack-delete-complete --stack-name $1
+echo "stack $1 deleted successfully"
