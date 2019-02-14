fc2a6c09932b218c3e20f8b1c901a43753b51de1
Gaurang Davda <davda.g@husky.neu.edu>
2019-02-14 12:54:14 -0500

    unauthorized error message

--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/impl/NoteTakingServiceImpl.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/impl/NoteTakingServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import edu.neu.csye6225.spring19.cloudninja.service.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +15,7 @@ import edu.neu.csye6225.spring19.cloudninja.model.UserCredentials;
 import edu.neu.csye6225.spring19.cloudninja.repository.NoteTakingRepository;
 import edu.neu.csye6225.spring19.cloudninja.service.AuthService;
 import edu.neu.csye6225.spring19.cloudninja.service.NoteTakingService;
+import edu.neu.csye6225.spring19.cloudninja.service.ValidationService;
 
 @Service
 public class NoteTakingServiceImpl implements NoteTakingService {
@@ -29,10 +29,8 @@ public class NoteTakingServiceImpl implements NoteTakingService {
 	@Autowired
 	private ValidationService validationService;
 
-
 	@Override
-	public List<Note> getAllNotes(String authHeader)
-			throws ValidationException, UnAuthorizedLoginException {
+	public List<Note> getAllNotes(String authHeader) throws ValidationException, UnAuthorizedLoginException {
 		UserCredentials credentials = authService.extractCredentialsFromHeader(authHeader);
 		authService.authenticateUser(credentials);
 		List<Note> noteList = noteTakingRepository.getAllNotesForUser(credentials);
@@ -40,8 +38,7 @@ public class NoteTakingServiceImpl implements NoteTakingService {
 	}
 
 	@Override
-	public Note createNote(String authHeader, Note note)
-			throws ValidationException, UnAuthorizedLoginException {
+	public Note createNote(String authHeader, Note note) throws ValidationException, UnAuthorizedLoginException {
 		UserCredentials credentials = authService.extractCredentialsFromHeader(authHeader);
 		authService.authenticateUser(credentials);
 		validationService.isNoteValid(note);
@@ -55,16 +52,16 @@ public class NoteTakingServiceImpl implements NoteTakingService {
 			throws ValidationException, UnAuthorizedLoginException, ResourceNotFoundException {
 		UserCredentials credentials = authService.extractCredentialsFromHeader(authHeader);
 		authService.authenticateUser(credentials);
-		//Note note = fetchNoteFromId(noteId);
+		// Note note = fetchNoteFromId(noteId);
 		Optional<Note> noteWrapper = noteTakingRepository.findById(noteId);
-		if(!noteWrapper.isPresent() || noteWrapper.get() == null) {
+		if (!noteWrapper.isPresent() || noteWrapper.get() == null) {
 			throw new ResourceNotFoundException("Note with ID: " + noteId.toString() + " does not exist");
 		}
 		Note note = noteWrapper.get();
 		if (authService.isUserAuthorized(credentials, note)) {
 			return note;
 		} else {
-			throw new UnAuthorizedLoginException();
+			throw new UnAuthorizedLoginException("Note with ID: " + noteId.toString() + " is not one of your notes");
 		}
 	}
 
@@ -74,44 +71,43 @@ public class NoteTakingServiceImpl implements NoteTakingService {
 		UserCredentials credentials = authService.extractCredentialsFromHeader(authHeader);
 		authService.authenticateUser(credentials);
 		validationService.isNoteValid(note);
-		//Note nt = fetchNoteFromId(noteId);
+		// Note nt = fetchNoteFromId(noteId);
 		Optional<Note> noteWrapper = noteTakingRepository.findById(noteId);
-		if(!noteWrapper.isPresent() || noteWrapper.get() == null) {
+		if (!noteWrapper.isPresent() || noteWrapper.get() == null) {
 			throw new ValidationException("Note with ID: " + noteId.toString() + " does not exist");
 		}
 		Note nt = noteWrapper.get();
-		
-		if(authService.isUserAuthorized(credentials, nt)) {
+
+		if (authService.isUserAuthorized(credentials, nt)) {
 			nt.setContent(note.getContent());
 			nt.setTitle(note.getTitle());
 			noteTakingRepository.save(nt);
 		} else {
-			throw new UnAuthorizedLoginException();
+			throw new UnAuthorizedLoginException("Note with ID: " + noteId.toString() + " is not one of your notes");
 		}
 	}
 
 	@Override
-	public void deleteNote(String authHeader, UUID noteId)
-			throws ValidationException, UnAuthorizedLoginException {
+	public void deleteNote(String authHeader, UUID noteId) throws ValidationException, UnAuthorizedLoginException {
 		UserCredentials credentials = authService.extractCredentialsFromHeader(authHeader);
 		authService.authenticateUser(credentials);
-		
-		//Note note = fetchNoteFromId(noteId);
-		
+
+		// Note note = fetchNoteFromId(noteId);
+
 		Optional<Note> noteWrapper = noteTakingRepository.findById(noteId);
-		if(!noteWrapper.isPresent() || noteWrapper.get() == null) {
+		if (!noteWrapper.isPresent() || noteWrapper.get() == null) {
 			throw new ValidationException("Note with ID: " + noteId.toString() + " does not exist");
 		}
 		Note note = noteWrapper.get();
 		if (authService.isUserAuthorized(credentials, note)) {
 			noteTakingRepository.delete(note);
 		} else {
-			throw new UnAuthorizedLoginException();
+			throw new UnAuthorizedLoginException("Note with ID: " + noteId.toString() + " is not one of your notes");
 		}
 
 	}
 
-	//Removing since we need seperate HTTP response codes for GET,PUT, DELETE
+	// Removing since we need seperate HTTP response codes for GET,PUT, DELETE
 //	private Note fetchNoteFromId(UUID noteId) throws ResourceNotFoundException {
 //		Optional<Note> noteWrapper = noteTakingRepository.findById(noteId);
 //		if(!noteWrapper.isPresent() || noteWrapper.get() == null) {
