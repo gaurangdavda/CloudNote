c593157221acf79fd9d744953aea3fa90b962d62
Gaurang Davda <davda.g@husky.neu.edu>
2019-03-11 21:49:01 -0400

    adding loggers in webapp

--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/impl/LoginServiceImpl.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/impl/LoginServiceImpl.java
@@ -3,6 +3,9 @@ package edu.neu.csye6225.spring19.cloudninja.service.impl;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +18,16 @@ import edu.neu.csye6225.spring19.cloudninja.repository.UserRepository;
 import edu.neu.csye6225.spring19.cloudninja.service.AuthService;
 import edu.neu.csye6225.spring19.cloudninja.service.LoginService;
 import edu.neu.csye6225.spring19.cloudninja.util.AuthServiceUtil;
+import edu.neu.csye6225.spring19.cloudninja.util.CommonUtil;
 
 @Service
 public class LoginServiceImpl implements LoginService {
 
 	@Autowired
 	private AuthServiceUtil authServiceUtil;
+
+	@Autowired
+	private CommonUtil commonUtil;
 
 	@Autowired
 	private UserRepository userRepository;
@@ -34,32 +41,46 @@ public class LoginServiceImpl implements LoginService {
 	@Autowired
 	private AuthService authService;
 
+	private static final Logger logger = LogManager.getLogger(LoginServiceImpl.class);
+
 	@Override
 	public TimeStampWrapper getTimestamp(String authHeader) throws ValidationException, UnAuthorizedLoginException {
 		// Authenticating User before proceeding
-		authService.authenticateUser(authService.extractCredentialsFromHeader(authHeader));
-		timeStampWrapper.setTimeStamp(
-				new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date(System.currentTimeMillis())));
-		return timeStampWrapper;
+		try {
+			authService.authenticateUser(authService.extractCredentialsFromHeader(authHeader));
+
+			timeStampWrapper.setTimeStamp(
+					new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date(System.currentTimeMillis())));
+			logger.log(Level.INFO, "Login successful.");
+			return timeStampWrapper;
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
+		}
 	}
 
 	public ResponseBody registerUser(UserCredentials userCredential) throws ValidationException {
 
-		authServiceUtil.isValidEmail(userCredential.getEmailId());
-		String emailId = userCredential.getEmailId().toLowerCase();
+		try {
+			authServiceUtil.isValidEmail(userCredential.getEmailId());
+			String emailId = userCredential.getEmailId().toLowerCase();
 
-		List<UserCredentials> credentialList = userRepository.findByEmailId(emailId);
-		if (credentialList == null || credentialList.size() == 0) {
-			authServiceUtil.checkPasswordStrength(userCredential.getPassword());
-			String password = authServiceUtil.encryptPassword(userCredential.getPassword());
-			userCredential.setEmailId(emailId);
-			userCredential.setPassword(password);
-			userRepository.save(userCredential);
-		} else {
-			throw new ValidationException("User already exists. Kindly login.");
+			List<UserCredentials> credentialList = userRepository.findByEmailId(emailId);
+			if (credentialList == null || credentialList.size() == 0) {
+				authServiceUtil.checkPasswordStrength(userCredential.getPassword());
+				String password = authServiceUtil.encryptPassword(userCredential.getPassword());
+				userCredential.setEmailId(emailId);
+				userCredential.setPassword(password);
+				userRepository.save(userCredential);
+			} else {
+				throw new ValidationException("User already exists. Kindly login.");
+			}
+			responseBody.setResponseMessage("User created successfully.");
+			return responseBody;
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
 		}
-		responseBody.setResponseMessage("User created successfully.");
-		return responseBody;
 	}
 
 }
--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/impl/NoteTakingServiceImpl.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/service/impl/NoteTakingServiceImpl.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -45,125 +48,175 @@ public class NoteTakingServiceImpl implements NoteTakingService {
 	@Autowired
 	private CommonUtil commonUtil;
 
+	private static final Logger logger = LogManager.getLogger(NoteTakingServiceImpl.class);
+
 	@Override
 	public List<Note> getAllNotes(String auth) throws ValidationException, UnAuthorizedLoginException {
-		UserCredentials credentials = authService.extractCredentialsFromHeader(auth);
-		authService.authenticateUser(credentials);
-		List<Note> noteList = noteTakingRepository.getAllNotesForUser(credentials);
-		return noteList;
+		try {
+			UserCredentials credentials = authService.extractCredentialsFromHeader(auth);
+			authService.authenticateUser(credentials);
+			List<Note> noteList = noteTakingRepository.getAllNotesForUser(credentials);
+			return noteList;
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
+		}
 	}
 
 	@Override
 	public Note createNote(String auth, Note note) throws ValidationException, UnAuthorizedLoginException {
-		UserCredentials credentials = authService.extractCredentialsFromHeader(auth);
-		authService.authenticateUser(credentials);
-		validationService.isNoteValid(note);
-		note.setUserCredentials(credentials);
-		Note savedNote = noteTakingRepository.save(note);
-		return savedNote;
+		try {
+			UserCredentials credentials = authService.extractCredentialsFromHeader(auth);
+			authService.authenticateUser(credentials);
+			validationService.isNoteValid(note);
+			note.setUserCredentials(credentials);
+			Note savedNote = noteTakingRepository.save(note);
+			return savedNote;
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
+		}
 	}
 
 	@Override
 	public Note getNote(String auth, UUID noteId)
 			throws ValidationException, UnAuthorizedLoginException, ResourceNotFoundException {
-		UserCredentials credentials = authService.extractCredentialsFromHeader(auth);
-		authService.authenticateUser(credentials);
+		try {
+			UserCredentials credentials = authService.extractCredentialsFromHeader(auth);
+			authService.authenticateUser(credentials);
 
-		Optional<Note> noteWrapper = noteTakingRepository.findById(noteId);
-		if (!noteWrapper.isPresent() || noteWrapper.get() == null) {
-			throw new ResourceNotFoundException("Note with ID: " + noteId.toString() + " does not exist");
-		}
-		Note note = noteWrapper.get();
-		if (authService.isUserAuthorized(credentials, note)) {
-			return note;
-		} else {
-			throw new UnAuthorizedLoginException("Note with ID: " + noteId.toString() + " is not one of your notes");
+			Optional<Note> noteWrapper = noteTakingRepository.findById(noteId);
+			if (!noteWrapper.isPresent() || noteWrapper.get() == null) {
+				throw new ResourceNotFoundException("Note with ID: " + noteId.toString() + " does not exist");
+			}
+			Note note = noteWrapper.get();
+			if (authService.isUserAuthorized(credentials, note)) {
+				return note;
+			} else {
+				throw new UnAuthorizedLoginException(
+						"Note with ID: " + noteId.toString() + " is not one of your notes");
+			}
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
 		}
 	}
 
 	@Override
 	public void updateNote(String auth, UUID noteId, Note note)
 			throws ValidationException, UnAuthorizedLoginException, ResourceNotFoundException {
-		validationService.isNoteValid(note);
-		Note fetchedNote;
-		fetchedNote = getNote(auth, noteId);
-		fetchedNote.setContent(note.getContent());
-		fetchedNote.setTitle(note.getTitle());
-		noteTakingRepository.save(fetchedNote);
+		try {
+			validationService.isNoteValid(note);
+			Note fetchedNote;
+			fetchedNote = getNote(auth, noteId);
+			fetchedNote.setContent(note.getContent());
+			fetchedNote.setTitle(note.getTitle());
+			noteTakingRepository.save(fetchedNote);
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
+		}
 	}
 
 	@Override
 	public void deleteNote(String auth, UUID noteId)
 			throws ValidationException, UnAuthorizedLoginException, ResourceNotFoundException, FileStorageException {
-
-		Note fetchedNote;
-		fetchedNote = getNote(auth, noteId);
-		List<Attachment> attachments = fetchedNote.getAttachments();
-		if (attachments != null && attachments.size() > 0) {
-			for (Attachment a : attachments) {
-				fileStorageUtil.deleteFile(a.getUrl());
-				attachmentReposiory.delete(a);
+		try {
+			Note fetchedNote;
+			fetchedNote = getNote(auth, noteId);
+			List<Attachment> attachments = fetchedNote.getAttachments();
+			if (attachments != null && attachments.size() > 0) {
+				for (Attachment a : attachments) {
+					fileStorageUtil.deleteFile(a.getUrl());
+					attachmentReposiory.delete(a);
+				}
 			}
+			noteTakingRepository.delete(fetchedNote);
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
 		}
-		noteTakingRepository.delete(fetchedNote);
 	}
 
 	@Override
 	public List<Attachment> getAttachments(String auth, UUID noteId)
 			throws ValidationException, UnAuthorizedLoginException, ResourceNotFoundException {
-		Note fetchedNote;
-		fetchedNote = getNote(auth, noteId);
-		return fetchedNote.getAttachments();
+		try {
+			Note fetchedNote;
+			fetchedNote = getNote(auth, noteId);
+			return fetchedNote.getAttachments();
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
+		}
 	}
 
 	@Override
 	public Attachment saveAttachment(String auth, UUID noteId, MultipartFile file)
 			throws ValidationException, UnAuthorizedLoginException, ResourceNotFoundException, FileStorageException {
-
-		Note fetchedNote;
-		fetchedNote = getNote(auth, noteId);
-		String fileLocation = fileStorageUtil.storeFile(file);
-		Attachment attachment = new Attachment(fileLocation, fetchedNote);
-		attachment.setFileName(commonUtil.getFileNameFromPath(fileLocation));
-		return attachmentReposiory.save(attachment);
+		try {
+			Note fetchedNote;
+			fetchedNote = getNote(auth, noteId);
+			String fileLocation = fileStorageUtil.storeFile(file);
+			Attachment attachment = new Attachment(fileLocation, fetchedNote);
+			attachment.setFileName(commonUtil.getFileNameFromPath(fileLocation));
+			return attachmentReposiory.save(attachment);
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
+		}
 	}
 
 	@Override
 	public List<Attachment> saveAttachments(String auth, UUID noteId, MultipartFile[] files)
 			throws ValidationException, UnAuthorizedLoginException, ResourceNotFoundException, FileStorageException {
-
-		Note fetchedNote;
-		fetchedNote = getNote(auth, noteId);
-		List<Attachment> savedAttachments = new ArrayList<Attachment>();
-		for (MultipartFile file : files) {
-			String fileLocation = fileStorageUtil.storeFile(file);
-			Attachment attachment = new Attachment(fileLocation, fetchedNote);
-			attachment.setFileName(commonUtil.getFileNameFromPath(fileLocation));
-			savedAttachments.add(attachmentReposiory.save(attachment));
+		try {
+			Note fetchedNote;
+			fetchedNote = getNote(auth, noteId);
+			List<Attachment> savedAttachments = new ArrayList<Attachment>();
+			for (MultipartFile file : files) {
+				String fileLocation = fileStorageUtil.storeFile(file);
+				Attachment attachment = new Attachment(fileLocation, fetchedNote);
+				attachment.setFileName(commonUtil.getFileNameFromPath(fileLocation));
+				savedAttachments.add(attachmentReposiory.save(attachment));
+			}
+			return savedAttachments;
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
 		}
-		return savedAttachments;
 	}
 
 	@Override
 	public void updateAttachment(String auth, UUID noteId, UUID attachmentId, MultipartFile file)
 			throws ValidationException, UnAuthorizedLoginException, FileStorageException, ResourceNotFoundException {
-		Note fetchedNote;
-		fetchedNote = getNote(auth, noteId);
-		Attachment attachment = getAttachmentForNote(attachmentId, fetchedNote);
-		String fileLocation = fileStorageUtil.replaceFile(attachment.getUrl(), file);
-		attachment.setUrl(fileLocation);
-		attachment.setFileName(commonUtil.getFileNameFromPath(fileLocation));
-		attachmentReposiory.save(attachment);
+		try {
+			Note fetchedNote;
+			fetchedNote = getNote(auth, noteId);
+			Attachment attachment = getAttachmentForNote(attachmentId, fetchedNote);
+			String fileLocation = fileStorageUtil.replaceFile(attachment.getUrl(), file);
+			attachment.setUrl(fileLocation);
+			attachment.setFileName(commonUtil.getFileNameFromPath(fileLocation));
+			attachmentReposiory.save(attachment);
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
+		}
 	}
 
 	@Override
 	public void deleteAttachment(String auth, UUID noteId, UUID attachmentId)
 			throws ValidationException, UnAuthorizedLoginException, FileStorageException, ResourceNotFoundException {
-		Note fetchedNote;
-		fetchedNote = getNote(auth, noteId);
-		Attachment attachment = getAttachmentForNote(attachmentId, fetchedNote);
-		fileStorageUtil.deleteFile(attachment.getUrl());
-		attachmentReposiory.delete(attachment);
+		try {
+			Note fetchedNote;
+			fetchedNote = getNote(auth, noteId);
+			Attachment attachment = getAttachmentForNote(attachmentId, fetchedNote);
+			fileStorageUtil.deleteFile(attachment.getUrl());
+			attachmentReposiory.delete(attachment);
+		} catch (Exception e) {
+			logger.log(Level.ERROR, commonUtil.stackTraceString(e));
+			throw e;
+		}
 	}
 
 	private Attachment getAttachmentForNote(UUID attachmentId, Note fetchedNote)
@@ -173,11 +226,6 @@ public class NoteTakingServiceImpl implements NoteTakingService {
 			throw new ValidationException("Attachment with ID: " + attachmentId.toString() + " does not exist");
 		}
 		Attachment attachment = attachmentWrapper.get();
-//		for (Attachment a : fetchedNote.getAttachments()) {
-//			if (a.getId().equals(attachment.getId())) {
-//				return attachment;
-//			}
-//		}
 		if (fetchedNote.getAttachments().contains(attachment)) {
 			return attachment;
 		}
