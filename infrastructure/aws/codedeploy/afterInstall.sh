ea3810e6205fd371d4529170e69e1a8e629f0714
vivdalal <dalal.vi@husky.neu.edu>
2019-03-13 01:05:03 -0400

    updated jar run command

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Commands to run after after installation
-java -jar cloud-ninja-SNAPSHOT.jar -Dspring.profiles.active=dev -DamazonProperties.bucketName=${S3_BUCKET_NAME} -Dspring.datasource.url=${DB_ENDPOINT} -Dspring.datasource.username=${DB_USERNAME} -Dspring.datasource.password=${DB_PASSWORD}
+nohup java -jar cloud-ninja-SNAPSHOT.jar -Dspring.profiles.active=dev &
