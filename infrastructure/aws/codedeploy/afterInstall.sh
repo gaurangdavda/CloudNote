b9c32ec9acc5cbbb7f54ed0f7e662e9218d266db
vivdalal <dalal.vi@husky.neu.edu>
2019-03-14 02:25:02 -0400

    Handling output from jar run command

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -6,9 +6,5 @@ sudo chown -R centos:centos /home/centos/webapp/*
 sudo chmod +x cloud-ninja-SNAPSHOT.jar
 kill -9 $(ps -ef|grep cloud-ninja | grep -v grep | awk '{print $2}')
 source /etc/profile.d/envvariable.sh
-
-rm -rf ./logs
-
-mkdir logs
-nohup java -Dspring.profiles.active=dev -jar cloud-ninja-SNAPSHOT.jar > /dev/null 2>&1
+java -Dspring.profiles.active=dev -jar cloud-ninja-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 echo "Leaving after install hook"
