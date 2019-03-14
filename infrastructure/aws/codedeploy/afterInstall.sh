5bd8c09e99e756292e3f868b7840785979db8685
vivdalal <dalal.vi@husky.neu.edu>
2019-03-14 04:14:21 -0400

    spring profile in user data

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -6,5 +6,5 @@ sudo chown -R centos:centos /home/centos/webapp/*
 sudo chmod +x cloud-ninja-SNAPSHOT.jar
 kill -9 $(ps -ef|grep cloud-ninja | grep -v grep | awk '{print $2}')
 source /etc/profile.d/envvariable.sh
-nohup java -Dspring.profiles.active=dev -jar cloud-ninja-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar cloud-ninja-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 echo "Leaving after install hook"
