6ef99135ddf574daae96036eaf88d1f3171ad899
vivdalal <dalal.vi@husky.neu.edu>
2019-03-14 03:28:22 -0400

    Removed unnecessary command

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -2,7 +2,6 @@
 #Commands to run after after installation
 echo "Entered after install hook"
 cd /home/centos/webapp
-sudo chown -R centos:centos /home/centos/webapp
 sudo chown -R centos:centos /home/centos/webapp/*
 sudo chmod +x cloud-ninja-SNAPSHOT.jar
 kill -9 $(ps -ef|grep cloud-ninja | grep -v grep | awk '{print $2}')
