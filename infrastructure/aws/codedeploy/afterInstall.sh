a043aeef1f3ba793cb76dbaf76402f8dcb47a3db
vivdalal <dalal.vi@husky.neu.edu>
2019-03-14 02:06:09 -0400

    Handling output from jar run command

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -4,7 +4,7 @@ echo "Entered after install hook"
 cd /home/centos/webapp
 sudo chown -R centos:centos /home/centos/webapp/*
 sudo chmod +x cloud-ninja-SNAPSHOT.jar
-kill -9 $(ps -ef|grep cloud-ninja | grep -v grep | awk '{print $2}')
+#kill -9 $(ps -ef|grep cloud-ninja | grep -v grep | awk '{print $2}')
 source /etc/profile.d/envvariable.sh
 touch app-run-out.log
 touch app-run-error.log
