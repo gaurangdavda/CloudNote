658cc17625c5814e1afaf6dcd1aa6da41aba3831
Gaurang Davda <davda.g@husky.neu.edu>
2019-03-16 18:03:34 -0400

    trying jdk 9

--- webapp/cloud-ninja/src/test/java/edu/neu/csye6225/spring19/cloudninja/CloudNinjaApplicationTests.java
+++ webapp/cloud-ninja/src/test/java/edu/neu/csye6225/spring19/cloudninja/CloudNinjaApplicationTests.java
@@ -1,9 +1,12 @@
 package edu.neu.csye6225.spring19.cloudninja;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class CloudNinjaApplicationTests {
 
