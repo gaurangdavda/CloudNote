7978a9392ccdc082d224f86cd06ffadb19637dfa
vivdalal <dalal.vi@husky.neu.edu>
2019-03-20 20:40:35 -0400

    removing unused imports

--- webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/metrics/MethodProfiler.java
+++ webapp/cloud-ninja/src/main/java/edu/neu/csye6225/spring19/cloudninja/metrics/MethodProfiler.java
@@ -7,7 +7,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
